6e0a1e32b9a776484530f6d46edefb58c200964d
morihiroto <arigatoeggplant@gmail.com>
2022-01-19 17:09:39 +0900

    3秒後に送信完了する定で、onCompleteに依存したイベントを設定

--- app/src/main/java/com/hirogram/validatesample/MainActivity.kt
+++ app/src/main/java/com/hirogram/validatesample/MainActivity.kt
@@ -2,13 +2,21 @@ package com.hirogram.validatesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.hirogram.validatesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding :ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.form = MainForm()
+
+        val form = MainForm()
+        form.onComplete.observe(this, Observer {
+            Toast.makeText(this, "送信しました", Toast.LENGTH_SHORT).show()
+            finish()
+        })
+        binding.form = form
     }
 }
--- app/src/main/java/com/hirogram/validatesample/MainForm.kt
+++ app/src/main/java/com/hirogram/validatesample/MainForm.kt
@@ -1,28 +1,34 @@
 package com.hirogram.validatesample
 
-import android.content.Context
-import android.widget.Toast
+import android.os.Handler
+import android.os.Looper
 import androidx.databinding.BaseObservable
 import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
 
 class MainForm : BaseObservable() {
     val to = ObservableField("")
     val subject = ObservableField("")
     val message = ObservableField("")
     val valid = ObservableBoolean(true)
+    val requesting = ObservableBoolean()
 
-    fun validate(context: Context) {
+    val onComplete = MutableLiveData<Boolean>()
+
+    fun validate() {
         val result = !to.get().isNullOrBlank()
+        valid.set(result)
         if (result) {
-            send(context)
-        } else {
-            valid.set(result)
+            requesting.set(true)
+            send()
         }
     }
 
-    private fun send(context: Context) {
-        Toast.makeText(context, message.get(), Toast.LENGTH_SHORT).show()
+    private fun send() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            onComplete.postValue(true)
+        }, 3000)
     }
 
 }

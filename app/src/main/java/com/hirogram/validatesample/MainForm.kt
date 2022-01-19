f85e927de399e7fb18209eccb5330bc45a6ea419
morihiroto <arigatoeggplant@gmail.com>
2022-01-19 16:13:52 +0900

    BindingAdapterの引数を2つに変更

--- app/src/main/java/com/hirogram/validatesample/MainForm.kt
+++ app/src/main/java/com/hirogram/validatesample/MainForm.kt
@@ -3,22 +3,21 @@ package com.hirogram.validatesample
 import android.content.Context
 import android.widget.Toast
 import androidx.databinding.BaseObservable
+import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
 
 class MainForm : BaseObservable() {
     val to = ObservableField("")
     val subject = ObservableField("")
     val message = ObservableField("")
-    val errorMessage = ObservableField<String>()
+    val valid = ObservableBoolean(true)
 
     fun validate(context: Context) {
         val result = !to.get().isNullOrBlank()
-
-        val error = if (result) null else "宛先を必ず指定してください。"
-            errorMessage.set(error)
-
         if (result) {
             send(context)
+        } else {
+            valid.set(result)
         }
     }
 
